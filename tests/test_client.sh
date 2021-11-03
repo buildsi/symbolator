ade1f025b9c0091a7f77253e15dcf3a0b3a986ef
vsoch <vsoch@users.noreply.github.com>
2021-11-03 15:30:45 -0600

    add runtest function directly to file
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- tests/test_client.sh
+++ tests/test_client.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 
 # Include help functions
-. helpers.sh
+runTest() {
+
+    # The first argument is the code we should get
+    ERROR="${1:-}"
+    shift
+    OUTPUT=${1:-}
+    shift
+
+    "$@" > "${OUTPUT}" 2>&1
+    RETVAL="$?"
+
+    if [ "$ERROR" = "0" -a "$RETVAL" != "0" ]; then
+        echo "$@ (retval=$RETVAL) ERROR"
+        cat ${OUTPUT}
+        echo "Output in ${OUTPUT}"
+        exit 1
+    elif [ "$ERROR" != "0" -a "$RETVAL" = "0" ]; then
+        echo "$@ (retval=$RETVAL) ERROR"
+        echo "Output in ${OUTPUT}"
+        cat ${OUTPUT}
+        exit 1
+    else
+        echo "$@ (retval=$RETVAL) OK"
+    fi
+}
 
 echo
 echo "************** START: test_client.sh **********************"
